9a51b2f49d34232796afb84b9b5e7296a007ccf5
Guzel_Ayupova <guzel_robertovna@mail.ru>
2023-06-11 23:48:28 +0500

    Программа по вычислению суммы натуральных чисел в заданном диапазоне

---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int MinNumber = Prompt("Задайте минимальное значение : ");
+int MaxNumber = Prompt("Задайте максимальное значение : ");
+Console.WriteLine($"Сумма натуральных чисел в диапазоне от {MinNumber} до {MaxNumber}: {NaturalNumber(0, MinNumber, MaxNumber)}");
+
+
+int NaturalNumber(int summa, int minValue, int maxValue)
+{
+    if (minValue > maxValue || maxValue <= 0) return summa;
+    else if (minValue <= 0) minValue = 1;
+    summa += minValue;
+    minValue++;
+    int sum = NaturalNumber(summa, minValue, maxValue);
+    return sum;
+}
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
