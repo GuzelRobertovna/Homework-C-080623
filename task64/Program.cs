382953ca7c338eb85bc78eec5df61b02834b3809
Guzel_Ayupova <guzel_robertovna@mail.ru>
2023-06-11 23:46:28 +0500

    Программа по выводу натуральных чисел в заданном диапазоне

---
+++ task64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+int MinNumber = Prompt("Задайте минимальное значение : ");
+int MaxNumber = Prompt("Задайте максимальное значение : ");
+Console.WriteLine($"Натуральные числа в диапазоне от {MinNumber} до {MaxNumber}:");
+NaturalNumber(MinNumber, MaxNumber);
+Console.WriteLine();
+
+
+void NaturalNumber(int minValue, int maxValue)
+{
+    if (minValue > maxValue || maxValue <= 0) return;
+    else if (minValue <= 0) minValue = 1;
+    Console.Write($"{minValue} ");
+    minValue++;
+    NaturalNumber(minValue, maxValue);
+}
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
